043ba92da89310dab17a8ce291831aa11d0d6aaf
Sidney Michiels <416488@student.fontys.nl>
2018-12-18 15:04:18 +0100

    renamed controls and added new game button

--- Breakout/Breakout/Form1.cs
+++ Breakout/Breakout/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Breakout
 {
-    public partial class Form1 : Form
+    public partial class frmMenu : Form
     {
-        public Form1()
+        public frmMenu()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
